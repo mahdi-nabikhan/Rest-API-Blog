c9ad78538f8fa5cd62805ca1e7f4f3348b4526d3
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2024-10-27 15:17:33 +0330

    add related_url for each post on post serializer

--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -9,6 +9,17 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_relative_api_url', read_only=True)
+
+    # absolute_url = serializers.SerializerMethodField(source='get_absolute_url', read_only=True)
+
     class Meta:
         model = Posts
-        fields = '__all__'
+        fields = (
+            'id', 'title', 'content', 'category', 'status', 'created_at', 'published_at', 'snippet', 'relative_url',
+            )
+
+    # def get_absolute_url(self, obj):
+    #     request = self.context.get('request')
+    #     return request.build_absolute_uri(obj.absolute_url)
--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -153,11 +153,7 @@ class PostCRUDViewSet(viewsets.ViewSet):
     def retrieve(self, request, pk):
         obj = Posts.objects.get(pk=pk)
         serializer = self.serializer_class(obj)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
 
     def create(self, request):
         data = request.data
